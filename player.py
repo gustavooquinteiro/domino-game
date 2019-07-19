f4e923004fc5afb8f5f26614daf5e46af8264044
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-07-18 21:16:51 -0300

    Adicionando a classe de jogador

---
+++ player.py
@@ -0,0 +1,41 @@
+class Player():
+    def __init__(self, nome):
+        self.nome = nome
+        self.pecas = []
+        self.move = None
+    
+    def set_pecas(self, peca):
+        self.pecas.append(peca)
+        
+    def __repr__(self):
+        return "{} com {}" .format(self.nome, self.pecas)
+    
+    def startable(self):
+        for peca in self.pecas:
+            if peca.is_startable():
+                self.move = peca
+                self.pecas.remove(self.move)
+                return True
+        return False
+        
+    def jogar_pedra(self, tabuleiro):
+        print(*self.pecas, sep='\n')
+        for pedra in self.pecas:
+            if tabuleiro.right_edge() in pedra:
+                if pedra.second == tabuleiro.right_edge():
+                    pedra.invert()
+                self.move = pedra
+                self.pecas.remove(self.move)
+                return True, True
+            if tabuleiro.left_edge() in pedra:
+                if pedra.first == tabuleiro.left_edge():
+                    pedra.invert()
+                self.move = pedra
+                self.pecas.remove(self.move)
+                return True, False
+        return False, None
+    
+    def ganhador(self):
+        return len(self.pecas) == 0
+
+    
