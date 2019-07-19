8955e8be04ce02ac2a96881666d005b2ca69841d
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-07-18 21:16:06 -0300

    Refatorando propriedades das peças

--- pecas.py
+++ pecas.py
@@ -1,12 +1,19 @@
 class Peca():
-    def __init__(self, numerador, denominador):
-        self.numerador = numerador
-        self.denominador = denominador
-    
-    def __repr__(self):
-        return "{} | {}" .format(self.numerador, self.denominador)
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
 
-    
- 
-p = Peca(1, 2)
-print(p)
+    def __repr__(self):
+        return "[{}|{}]" .format(self.first, self.second)
+
+    def __contains__(self, key):
+        return key == self.first or key == self.second
+
+    def __del__(self):
+        del self  
+
+    def invert(self):
+        self.first, self.second = self.second, self.first
+
+    def is_startable(self):
+        return self.first == 6 and self.second == 6
