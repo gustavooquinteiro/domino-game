9e4ee7bf0c1b29ad7b05fa609d82dd8944cf1cc2
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-07-21 12:32:34 -0300

    Criando a classe para o servidor

--- __init__.py
+++ __init__.py
@@ -2,24 +2,36 @@ from multiprocessing.connection import Listener
 from jogo import Jogo
 from jogo import Tabuleiro
 from player import Player
-import constants
+from constants import TUPLE
 
 
-server = Listener(constants.TUPLE)
-
-players = []
-try:
-    quantidade_players = int(input("Quantidade de players: "))
-except ValueError:
-    quantidade_players = 2
+class Server():
+    def __init__(self, board, quantidade_players=2, where_to_listen=TUPLE):
+        self.server = Listener(where_to_listen)
+        self.quantidade_players = quantidade_players
+        self.hosted_board = board
+        self.host_connected = []
+        self.wait_players()
+        
+    def wait_players(self):
+        while len(self.host_connected) != self.quantidade_players:
+            with self.server.accept() as connection:
+                client = connection.recv()
+                self.host_connected.append(client)
+                print("{} entered the server" .format(client))
+        self.run_game()
     
-while True:
-    con = server.accept()    
-    cliente = con.recv()
-    players.append(cliente)
-    print("{} entrou no server" .format(cliente))
-    if quantidade_players == len(players):
-        break
-    
-tabuleiro  = Tabuleiro()
-jogo = Jogo(players, tabuleiro)
+    def run_game(self):
+        self.host_connected = self.hosted_board.distribute(self.host_connected)
+        self.hosted_game = Jogo(self.host_connected, self.hosted_board)
+        while not self.hosted_game.ganhador:
+            self.hosted_game.play()
+        print("{} won..." .format(self.hosted_game.ganhador))
+
+if __name__ == "__main__":
+    tabuleiro  = Tabuleiro()
+    try:
+        quantidade_players = int(input("Quantidade de players: "))
+    except ValueError:
+        quantidade_players = 2
+    server = Server(tabuleiro, quantidade_players)
