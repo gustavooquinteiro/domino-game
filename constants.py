bdd1a634cbc1f4e4fc01a1c299ecf1774bb5f807
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-08-02 09:50:58 -0300

    Adicionando novas constantes

--- constants.py
+++ constants.py
@@ -1,4 +1,37 @@
- 
+
+"""
+Global constants
+"""
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+FPS = 60
+
 HOST = '127.0.0.1'
 PORT = 5000
 TUPLE = (HOST, PORT)
+
+# Screen dimensions
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
+
+#Title
+TITLE = "Jogo de Dominó"
+
+#Font
+FONT = "Arial"
+
+START = "Pressione Enter para jogar "
+GAME_OVER = b'You Lose'
+GAME_WIN = b'You win'
+CONNECTED = b'Player connected'
+WAITING = b'Waiting players'
+YOUR_TURN = b'Its your turn'
+WAITING_TURN = b'Waiting plays'
+
+MESSAGES = [CONNECTED, WAITING, YOUR_TURN, WAITING_TURN, GAME_OVER, GAME_WIN]
